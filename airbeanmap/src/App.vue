5369cb979133893aed45a88000852b79d3305825
Sinan-karaca <70816608+Sinan-karaca@users.noreply.github.com>
2021-02-15 10:55:59 +0100

    added name

--- airbeanmap/src/App.vue
+++ airbeanmap/src/App.vue
@@ -1,22 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
     <!-- felix test 1 -->
     <!-- fredrik test 1 -->
-    <!-- sinan test 2 -->
+    <!-- sinan test 3 -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
