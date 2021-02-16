458e6629ceeff82cace6a2287a662de096ff76f7
Sinan-karaca <70816608+Sinan-karaca@users.noreply.github.com>
2021-02-16 16:08:29 +0100

    la till bilder samt fixade vårtkaffe componenten lite

--- airbeanmap/src/views/Test.vue
+++ airbeanmap/src/views/Test.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Vartkaffe/>
   </div>
+  
 </template>
+
+<script>
+import Vartkaffe from '../components/Vartkaffe.vue'
+export default {
+  components: { Vartkaffe },
+  
+}
+</script>
+
+<style>
+
+</style>
