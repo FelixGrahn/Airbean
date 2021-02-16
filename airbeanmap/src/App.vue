26ad2780a9e76dcbd82000738e518ad8396f578d
felix.grahn <felix.grahn@outlook.com>
2021-02-16 13:25:13 +0100

    add-routing

--- airbeanmap/src/App.vue
+++ airbeanmap/src/App.vue
@@ -1,21 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" /> -->
     <!-- felix test 1 -->
     <!-- fredrik test 1 -->
     <!-- sinan test 3 -->
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+    </div>
+    <router-view />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+/* import HelloWorld from "./components/HelloWorld.vue"; */
 
 export default {
-  name: "App",
+  /* name: "App",
   components: {
     HelloWorld,
-  },
+  }, */
 };
 </script>
 
@@ -27,5 +31,18 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- airbeanmap/src/main.js
+++ airbeanmap/src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from "./router";
 
 Vue.config.productionTip = false
 
-new Vue({
+/* new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app') */
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount("#app");
