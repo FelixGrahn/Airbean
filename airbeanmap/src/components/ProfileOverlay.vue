02b017f5cda3da9e3e28728c0cd52d93e8a05528
fredrikpihlgren <fredrik.pihlgren@iths.se>
2021-03-03 09:34:49 +0100

    added form validation

--- airbeanmap/src/components/ProfileOverlay.vue
+++ airbeanmap/src/components/ProfileOverlay.vue
@@ -4,14 +4,21 @@
             <img src="@/assets/Group6.png" class="img" alt="image">
           <h2>Välkommen till AirBean-familjen!</h2>
           <h3>Genom att skapa ett konto nedan kan du spara och se din orderhistorik.</h3>
+
+            <div class="errorfield" v-if="errorlog.length > 0">
+                <ul>
+                <li v-for="(error, index) in errorlog" :key="index">{{error}}</li>
+                </ul>
+            </div>
+
             <form @submit.prevent @submit="login(email, password)">
 
             <label for="myname">Email</label>
             <input v-model="email" type="text" placeholder="Email" id="myname">
             <label for="myemail">Password</label>
-            <input v-model="password" type="password" placeholder="Password" id="myemail">
+            <input v-model="password" type="password" placeholder="Password" id="myemail" maxlength="20">
 
-            <input type="checkbox" id="gdpr" class="checkbox"><label for="gdpr" class="gdpr">GDPR OK</label>
+            <input type="checkbox" id="gdpr" class="checkbox" v-model="checked"><label for="gdpr" class="gdpr">GDPR OK</label>
 
             <input type="submit" value="Brew me a cup!" class="button">
 
@@ -29,11 +36,23 @@ export default {
         return {
             email: "",
             password: "",
+            errorlog: [],
+            checked: false,
         }
     },
     methods: {
         login: function(un, pw) {
-            this.$store.commit('login', {user:un, pass:pw});
+            this.errorlog=[];
+
+            if (this.email.length <= 0 || this.password <= 0) {
+                this.errorlog.push("Fields can't be blank.");
+            }
+            if (!this.checked) {
+                this.errorlog.push("You must accept DGPR.");
+            }
+
+            if (this.errorlog.length <= 0) {this.$store.commit('login', {user:un, pass:pw});}
+            
         }
         
         /*
@@ -52,6 +71,17 @@ export default {
 </script>
 
 <style scoped>
+
+.errorfield {
+    border:2px solid black;
+    background-color:white;
+    color:red;
+    width:70%;
+    min-height:100px;
+    margin-bottom:40px;
+    text-align:left;
+    font-weight:900;
+}
 
 h2 {
     width:90%;
